ffff462586b31d2bb7f4574e9c8813d18e32e4af
Mohan Kalyan Meduri <medurimk@iptrade-networks.com>
2026-06-10 14:26:59 +0530

    Added Selenium GitHub Actions setup

--- test_login.py
+++ test_login.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
 
 driver.get("https://www.cricbuzz.com")
 
-print(driver.title)
+print("Page Title:", driver.title)
+
+assert "Cricbuzz" in driver.title
 
 driver.quit()
+
+print("Test Passed")
